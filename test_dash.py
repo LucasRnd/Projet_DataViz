25a8da0f143ae4023fe1f615922a1658b0613b4b
Lucas <121796783+LucasRnd@users.noreply.github.com>
2024-01-31 23:13:34 +0100

    Update test_dash.py

--- test_dash.py
+++ test_dash.py
@@ -5,7 +5,7 @@ import numpy as np
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 import plotly.express as px
-import joblib
+#import joblib
 
 
 ########## Charger le modèle et le scaler ########
@@ -15,19 +15,16 @@ import joblib
 # Import du modèle Random Forest
 #best_rf_model = joblib.load('best_rf_model.pkl')
 
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import accuracy_score, classification_report
-from imblearn.over_sampling import SMOTE
-from sklearn.model_selection import GridSearchCV
-import joblib
 
 # Import des données
-df = pd.read_csv("C:/Users/Lucas/Downloads/archive/dataset_olympics.csv", sep=",", encoding="utf-8")
+url = "https://raw.githubusercontent.com/LucasRnd/Projet_DataViz/main/dataset_olympics.csv"
+df = pd.read_csv(url, sep=",", encoding="utf-8")
 
 # Filtrer les Jeux olympiques d'été depuis 1994
 df = df[(df['Year'] >= 1994) & (df['Year'] % 4 == 0)]
@@ -50,35 +47,31 @@ df['Medal'] = df['Medal'].replace(medal_mapping).fillna(0)
 X = df[['Age', 'Height', 'Weight']]
 y = df['Medal']
 
-# Suréchantillonnage des classes avec SMOTE
-smote = SMOTE(random_state=42)
-X_resampled, y_resampled = smote.fit_resample(X, y)
-
 # Diviser l'ensemble de données
-X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Standardiser les caractéristiques
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Initialiser le modèle de régression logistique avec des paramètres réduits
-logistic_model = LogisticRegression(random_state=42, C=1)
+# Initialiser le modèle de régression logistique avec des paramètres réduits et class_weight
+logistic_model = LogisticRegression(random_state=42, C=1, class_weight='balanced')
 
 # Entraîner le modèle
 logistic_model.fit(X_train_scaled, y_train)
 
 # Sauvegarder le modèle
-joblib.dump(logistic_model, 'best_logistic_model.pkl')
+#joblib.dump(logistic_model, 'best_logistic_model.pkl')
 
-# Initialiser le modèle de forêt aléatoire avec des paramètres réduits
-rf_model = RandomForestClassifier(random_state=42, n_estimators=100, max_depth=None)
+# Initialiser le modèle de forêt aléatoire avec des paramètres réduits et class_weight
+rf_model = RandomForestClassifier(random_state=42, n_estimators=100, max_depth=None, class_weight='balanced')
 
 # Entraîner le modèle
 rf_model.fit(X_train_scaled, y_train)
 
 # Sauvegarder le modèle
-joblib.dump(rf_model, 'best_rf_model.pkl')
+#joblib.dump(rf_model, 'best_rf_model.pkl')
 
 # Faire des prédictions sur l'ensemble de test
 y_pred_logistic = logistic_model.predict(X_test_scaled)
@@ -105,7 +98,8 @@ print(classification_rep_rf)
 
 ##########  Pré-traitement  ##########
 #Import des données
-df = pd.read_csv("C:/Users/Lucas/Downloads/archive/dataset_olympics.csv", sep=",", encoding="utf-8")
+url = "https://raw.githubusercontent.com/LucasRnd/Projet_DataViz/main/dataset_olympics.csv"
+df = pd.read_csv(url, sep=",", encoding="utf-8")
 
 #Filtrer les Jeux olympiques d'été depuis 1994
 df = df[(df['Year'] >= 1994) & (df['Year'] % 4 == 0)]
@@ -128,8 +122,8 @@ def generate_medailles_par_pays(selected_year):
                  labels={'y': 'Pays', 'x': 'Nombre de médailles'},
                  title=f'Top 10 des pays avec le plus grand nombre de médailles en {selected_year}')
     return(fig)
-   
-#Générer une BDD utile pour la carte & faire le graph 
+
+#Générer une BDD utile pour la carte & faire le graph
 def generate_world_map(selected_year):
     #Filtrer les données pour l'année sélectionnée
     filtered_df = df[df['Year'] == selected_year]
@@ -139,13 +133,13 @@ def generate_world_map(selected_year):
 
     #Créer une matrice pour l'affichage
     df_carte = filtered_df.groupby(['NOC', 'Team'])['Medal'].count().reset_index()
-    
+
     fig = px.scatter_geo(df_carte,
                          locations="NOC",  # Colonne contenant les codes des pays
                          size="Medal",  # Colonne contenant le nombre de médailles
                          projection="natural earth",  # Projection de la carte (d'autres options disponibles)
                          title=f"Carte du nombre de médailles par pays en {selected_year}")
-        
+
     fig.update_geos(showcoastlines=True, coastlinecolor="Black", showland=True, landcolor="white")
 
     return(fig)
@@ -170,7 +164,7 @@ app = dash.Dash(__name__)
 
 #Créer les sections de la première page
 page1_layout = html.Div(style={'backgroundColor': '#f2f2f2'}, children=[
-    
+
     ### Mettre un sommaire ###
     html.H1("Tableau de bord des Jeux olympiques"),
     # Sommaire
@@ -184,12 +178,12 @@ page1_layout = html.Div(style={'backgroundColor': '#f2f2f2'}, children=[
     # Section Accueil
     html.H2("Accueil", id='accueil'),
     html.P("Bienvenue dans le tableau de bord des Jeux olympiques. Explorez les données et découvrez des informations intéressantes."),
-    
-    
+
+
     ### Titre partie 1 ###
     html.H1("Partie I - Premier regard"),
-        
-        
+
+
     ### Medaille par annee ###
     dcc.Dropdown(
         id='annee-dropdown-page1',
@@ -214,8 +208,8 @@ html.P(
     "nombre élevé de médailles obtenus dans un sport. Également, ces jeux sont pratiqués à travers le "
     "monde et ont une grande cote de popularité et une visibilité médiatique importante."
 ),
-    
-    
+
+
     ### Double graph medaille par an + Carte des pays ###
     html.H1("Nombre de médaille par pays aux Jeux olympiques"),
     dcc.Dropdown(
@@ -232,7 +226,7 @@ html.P(
 
         # Carte mondiale interactive
         html.Div([dcc.Graph(id='world-map', style={'width': '100%'})], style={'flex': '1'}),
-     
+
     ], style={'display': 'flex'}),
     html.P(
         "En fonction de l’année sélectionnée, nous avons le top 10 des pays avec le plus grand nombre de "
@@ -249,11 +243,11 @@ html.P(
         "Janeiro). Idem pour le Royaume-Uni."
     ),
 
-         
-    
+
+
     ### Double graph participants + medailles ###
     html.H1("Nombre de participants et de médailles par année aux Jeux olympiques"),
-    
+
     # Composant interactif pour sélectionner la plage d'années
     dcc.RangeSlider(
         id='year-slider',
@@ -267,11 +261,11 @@ html.P(
     html.Div([
         # Graphique pour le nombre de participants
         html.Div([dcc.Graph(id='participants-par-annee')], style={'flex': '1'}),
-    
+
         # Graphique pour le nombre de médailles
         html.Div([dcc.Graph(id='medailles-par-annee2')], style={'flex': '1'}),
     ], style={'display': 'flex'}),
-    
+
      html.P(
         "Ce graphique permet de visualiser l’évolution du nombre de participants en fonction des années. Il "
         "suffit dans un premier temps de sélectionner l’intervalle d’année souhaité. Si l’on sélectionne toutes "
@@ -279,7 +273,7 @@ html.P(
         "nombre de participants se situait autour de 3400 et 3490. En 2012, ce nombre s’affaiblit à hauteur de "
         "3277 pour enfin arriver à 3598 en 2016."
     ),
-    
+
     html.P(
         "Ce graphique permet de visualiser le nombre de médailles par année. On peut voir une croissance "
         "quasiment linéaire de ce nombre. Cela peut être dû notamment à l’expansion des Jeux Olympiques. "
@@ -293,18 +287,18 @@ html.P(
 ])
 
 ######### Créer les sections de la deuxième page ###########
-page2_layout = html.Div([  
+page2_layout = html.Div([
     # Titre
     html.H1("Partie 2 - Analyse de l'âge et des mensurations"),
-    
+
     # Section Description
     html.H2("Description", id='accueil'),
     html.P("Bienvenue dans la deuxième partie du tableau de bord des Jeux olympiques. Cette section est dédiée à l'analyse des données relatives à l'âge et aux mensurations des athlètes participant aux Jeux olympiques."),
-    
+
     html.P("L'objectif de cette analyse est de fournir des insights intéressants sur les tendances générales de l'âge, de la taille et du poids des athlètes, en se concentrant sur la distinction entre hommes et femmes."),
-    
+
     html.P("Explorez ces graphiques interactifs pour découvrir des informations utiles sur les caractéristiques physiques des athlètes dans différentes disciplines."),
-    
+
     #### Moyennes taille, poids et age par genre
     html.H1("Analyse des caractéristiques moyennes par sport et par année"),
 
@@ -316,12 +310,12 @@ page2_layout = html.Div([
         # Graphiques pour les hommes
         html.Div([
             html.H2("Concernant les hommes", style={'text-align': 'center'}),
-            
+
             dcc.Graph(id='average-height-chart-M', style={'width': '33%', 'display': 'inline-block'}),
             dcc.Graph(id='average-weight-chart-M', style={'width': '33%', 'display': 'inline-block'}),
             dcc.Graph(id='average-age-chart-M', style={'width': '33%', 'display': 'inline-block'}),
         ]),
-        
+
         # Commentaires pour les graphiques des hommes
         html.P("Les basketteurs ont la plus grande taille, suivis des volleyeurs et enfin des joueurs de water polo. Les athlètes les plus petits pratiquent le trampoline."),
         html.P("En termes de poids, les basketteurs, les handballeurs et les joueurs de water-polo sont les plus lourds, tandis que les athlètes de trampoline sont les plus légers."),
@@ -331,12 +325,12 @@ page2_layout = html.Div([
     # Graphiques pour les femmes
     html.Div([
         html.H2("Concernant les femmes", style={'text-align': 'center'}),
-        
+
         dcc.Graph(id='average-height-chart-F', style={'width': '33%', 'display': 'inline-block'}),
         dcc.Graph(id='average-weight-chart-F', style={'width': '33%', 'display': 'inline-block'}),
         dcc.Graph(id='average-age-chart-F', style={'width': '33%', 'display': 'inline-block'}),
     ]),
-    
+
     # Commentaires pour les graphiques des femmes
     html.P("Les basketteuses, les volleyeuses et les joueuses de beach-volley ont la plus grande taille, tandis que les gymnastes ont la plus petite."),
     html.P("En termes de poids, les basketteuses, les handballeuses et les volleyeuses sont les plus lourdes, tandis que les gymnastes sont les plus légères."),
@@ -352,26 +346,26 @@ page3_layout = html.Div([
     html.H1("Partie 3 - Prédiction de médaille"),
     # Section Description
     html.H2("Description", id='accueil'),
-    
+
     html.P("Bienvenue dans la troisième partie du tableau de bord des Jeux olympiques. Dans cette section, vous avez la possibilité de faire des prédictions de médailles pour un athlète en particulier."),
-    
+
     html.P("Utilisez les champs ci-dessous pour entrer l'âge, la taille et le poids de l'athlète. Ensuite, cliquez sur le bouton 'Prédire' pour obtenir les probabilités de remporter une médaille dans chaque catégorie (Bronze, Argent, Or) selon deux modèles de machine learning : Régression Logistique et Random Forest."),
-    
+
     dcc.Input(id='input-age', type='number', placeholder='Age'),
     dcc.Input(id='input-height', type='number', placeholder='Height'),
     dcc.Input(id='input-weight', type='number', placeholder='Weight'),
     html.Button('Prédire', id='predict-button'),
-    
+
     # Section pour Régression Logistique
     html.H2("Régression Logistique", id='regression-logistique'),
     html.Div(id='prediction-output-lr'),
     dcc.Graph(id='probability-bar-chart-lr'),  # Graphique à barres pour la Régression Logistique
-    
+
 html.Div("Le graphique ci-dessus illustre les chances de médailles suivant les mensurations et "
          "l'âge de l'utilisateur à l'aide d'une Régression Logistique. On remarque que les probabilités ne fluctuent pas "
          "beaucoup et semblent erronées, cela est dû à l'accuracy qui est de 0.3."),  # Commentaire sous le graphique
 
-    
+
     # Section pour Random Forest
     html.H2("Random Forest", id='random-forest'),
     html.Div(id='prediction-output-rf'),
@@ -419,19 +413,19 @@ def update_page(selected_page):
 def update_medailles_par_annee(selected_year):
     #Filtrer les données en fonction de l'année sélectionnée
     filtered_df = df[df['Year'] == selected_year]
-    
+
     #Agréger les données par sport et calculer la somme des médailles
     medailles_par_sport = filtered_df.groupby('Sport')['Medal'].count().reset_index()
-    
+
     #Trier les sports en fonction du nombre de médailles (du plus élevé au plus bas)
     medailles_par_sport = medailles_par_sport.sort_values(by='Medal', ascending=False)
-    
+
     #Sélectionner les 10 premiers sports
     top_10_sports = medailles_par_sport.head(10)
-    
+
     #Créer un graphique avec Plotly Express
     fig = px.bar(top_10_sports, x='Sport', y='Medal', title=f'10 sports avec le plus de médailles en {selected_year}')
-    
+
     return(fig)
 
 ### Callback pour mettre à jour le graphique du nombre de médailles par pays
@@ -442,9 +436,9 @@ def update_medailles_par_annee(selected_year):
 def update_medailles_par_pays(selected_year):
     # Utilisez la fonction pour générer le graphique du nombre de médailles par pays
     fig = generate_medailles_par_pays(selected_year)
-    
+
     return(fig)
-    
+
 ### Callback pour mettre à jour la carte mondiale en fonction de l'année sélectionnée
 @app.callback(
     Output('world-map', 'figure'),
@@ -453,7 +447,7 @@ def update_medailles_par_pays(selected_year):
 def update_world_map(selected_year):
     # Utilisez la fonction generate_world_map pour créer la carte pour l'année sélectionnée
     fig = generate_world_map(selected_year)
-    
+
     return fig
 
 
@@ -467,7 +461,7 @@ def update_world_map(selected_year):
 def update_graphs(selected_years):
     # Filtrer les données en fonction de la plage d'années sélectionnée
     filtered_df = df[(df['Year'] >= selected_years[0]) & (df['Year'] <= selected_years[1])]
-    
+
     ### Partie pour les participants ###
     # Exclure les lignes avec des valeurs 'Athlete' manquantes (nan)
     filtered_df = filtered_df.dropna(subset=['ID'])
@@ -483,7 +477,7 @@ def update_graphs(selected_years):
     ### Partie pour les médailles ###
     # Filtrer les données avec médailles
     filtered_df = filtered_df[filtered_df['Medal'].notna()]
-    
+
     # Agréger les données par année pour le nombre de médailles
     medailles_par_annee = filtered_df['Year'].value_counts().sort_index()
 
@@ -600,7 +594,7 @@ def predict_medal_with_probabilities_and_chart_rf(n_clicks, age, height, weight)
 
     return prediction_str_rf, fig_rf
 
-    
+
 ##########  Test  ##########
 if __name__ == '__main__':
     app.run_server(debug=True)
