803eca4b60bc390d574a682640b7d2d568a54256
Lucas <121796783+LucasRnd@users.noreply.github.com>
2024-01-31 20:24:28 +0100

    Update test_dash.py

--- test_dash.py
+++ test_dash.py
@@ -10,12 +10,98 @@ import joblib
 
 ########## Charger le modèle et le scaler ########
 # Import du modèle de régression logistique
-best_lr_model = joblib.load('best_logistic_model.pkl')
+#best_lr_model = joblib.load('best_logistic_model.pkl')
 
 # Import du modèle Random Forest
-best_rf_model = joblib.load('best_rf_model.pkl')
+#best_rf_model = joblib.load('best_rf_model.pkl')
 
-    
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score, classification_report
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import GridSearchCV
+import joblib
+
+# Import des données
+df = pd.read_csv("C:/Users/Lucas/Downloads/archive/dataset_olympics.csv", sep=",", encoding="utf-8")
+
+# Filtrer les Jeux olympiques d'été depuis 1994
+df = df[(df['Year'] >= 1994) & (df['Year'] % 4 == 0)]
+
+# Convertir les colonnes 'Height', 'Weight' et 'Age' en numérique
+df[['Height', 'Weight', 'Age']] = df[['Height', 'Weight', 'Age']].apply(pd.to_numeric, errors='coerce')
+
+# Supprimer les lignes avec des valeurs manquantes
+df = df.dropna(subset=['Height', 'Weight', 'Age', 'Sex'])
+
+# Remplacer les valeurs manquantes par la médiane
+imputer = SimpleImputer(strategy='median')
+X = imputer.fit_transform(df[['Age', 'Height', 'Weight']])
+
+# Convertir les étiquettes de médaille en valeurs numériques
+medal_mapping = {'Bronze': 1, 'Silver': 2, 'Gold': 3}
+df['Medal'] = df['Medal'].replace(medal_mapping).fillna(0)
+
+# Séparer les caractéristiques (X) et la cible (y)
+X = df[['Age', 'Height', 'Weight']]
+y = df['Medal']
+
+# Suréchantillonnage des classes avec SMOTE
+smote = SMOTE(random_state=42)
+X_resampled, y_resampled = smote.fit_resample(X, y)
+
+# Diviser l'ensemble de données
+X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
+
+# Standardiser les caractéristiques
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Initialiser le modèle de régression logistique avec des paramètres réduits
+logistic_model = LogisticRegression(random_state=42, C=1)
+
+# Entraîner le modèle
+logistic_model.fit(X_train_scaled, y_train)
+
+# Sauvegarder le modèle
+joblib.dump(logistic_model, 'best_logistic_model.pkl')
+
+# Initialiser le modèle de forêt aléatoire avec des paramètres réduits
+rf_model = RandomForestClassifier(random_state=42, n_estimators=100, max_depth=None)
+
+# Entraîner le modèle
+rf_model.fit(X_train_scaled, y_train)
+
+# Sauvegarder le modèle
+joblib.dump(rf_model, 'best_rf_model.pkl')
+
+# Faire des prédictions sur l'ensemble de test
+y_pred_logistic = logistic_model.predict(X_test_scaled)
+y_pred_rf = rf_model.predict(X_test_scaled)
+
+# Évaluer les performances des modèles
+accuracy_logistic = accuracy_score(y_test, y_pred_logistic)
+classification_rep_logistic = classification_report(y_test, y_pred_logistic)
+
+accuracy_rf = accuracy_score(y_test, y_pred_rf)
+classification_rep_rf = classification_report(y_test, y_pred_rf)
+
+# Afficher les performances des deux modèles
+print("Régression Logistique:")
+print(f'Accuracy: {accuracy_logistic}')
+print('Classification Report:')
+print(classification_rep_logistic)
+
+print("\nForêt Aléatoire:")
+print(f'Accuracy: {accuracy_rf}')
+print('Classification Report:')
+print(classification_rep_rf)
+
 
 ##########  Pré-traitement  ##########
 #Import des données
@@ -78,10 +164,6 @@ def generate_average_chart(selected_feature, selected_gender):
     return fig
 
 
-
-######## Charger le modele de regression ######
-
-
 ##########  Dash  ##########
 #Créer une application Dash
 app = dash.Dash(__name__)
@@ -119,6 +201,19 @@ page1_layout = html.Div(style={'backgroundColor': '#f2f2f2'}, children=[
     # Visualisation du nombre de médailles par année
     html.H2("Médailles par annee", id='medailles-par-annee-section'),
     dcc.Graph(id='medailles-par-annee-page1'),
+html.P(
+    "Ce graphique en barres présente la répartition du nombre de médailles par sport en fonction de "
+    "l’année sélectionnée. Chaque barre représente un sport différent. En 2016 par exemple, 64 médailles "
+    "ont récompensé les pratiquants de l’athlétisme, 46 médailles pour la natation… "
+    "En analysant le graphique, on peut conclure que certains sports ont été plus performants en termes "
+    "de médailles que d'autres comme l’athlétisme, la natation et l’aviron qui se hissent dans le podium "
+    "en 2016, 2012, 2008, 2004 et 2000. "
+    "Cela peut être dû à plusieurs raisons, notamment le nombre d’épreuves qui peut-être plus grand en "
+    "fonction des sports. L’athlétisme par exemple propose une variété d'épreuves de sprint, de saut, de "
+    "lancer, de demi-fond, etc. De ce fait, la diversité des épreuves peut être un élément justifiant le "
+    "nombre élevé de médailles obtenus dans un sport. Également, ces jeux sont pratiqués à travers le "
+    "monde et ont une grande cote de popularité et une visibilité médiatique importante."
+),
     
     
     ### Double graph medaille par an + Carte des pays ###
@@ -137,8 +232,24 @@ page1_layout = html.Div(style={'backgroundColor': '#f2f2f2'}, children=[
 
         # Carte mondiale interactive
         html.Div([dcc.Graph(id='world-map', style={'width': '100%'})], style={'flex': '1'}),
+     
     ], style={'display': 'flex'}),
-          
+    html.P(
+        "En fonction de l’année sélectionnée, nous avons le top 10 des pays avec le plus grand nombre de "
+        "médailles remporté. On peut observer visuellement quels pays ont eu le plus de succès en termes de "
+        "médailles. L'analyse de ce graphique permet de tirer des conclusions sur les performances relatives "
+        "des différents pays en termes de médailles. Par exemple en 2016, c’est le Brésil (229) qui se hissent à "
+        "la première place suivie de près par les États-Unis (220). En 2012, c’est les USA qui remportent la "
+        "première place avec 170 médailles. "
+        "On peut prétendre en général l’existence d’une corrélation entre les performances sportives d'un "
+        "pays hôte aux Jeux Olympiques et le fait que ce pays se classe dans le top 10 des médailles. Par "
+        "exemple, la Grèce qui ne fait généralement pas partie du Top 10, s’est classée à la dixième place lors "
+        "des JO 2004 organisés à Athènes. Le Brésil classé 9 ème en 1996, puis 10 ème en 2000, 7 ème en 2004, 2008 "
+        "et 2012, s’est hissé à la première place en 2016 (année à laquelle les JO se sont déroulés à Rio de "
+        "Janeiro). Idem pour le Royaume-Uni."
+    ),
+
+         
     
     ### Double graph participants + medailles ###
     html.H1("Nombre de participants et de médailles par année aux Jeux olympiques"),
@@ -161,17 +272,39 @@ page1_layout = html.Div(style={'backgroundColor': '#f2f2f2'}, children=[
         html.Div([dcc.Graph(id='medailles-par-annee2')], style={'flex': '1'}),
     ], style={'display': 'flex'}),
     
+     html.P(
+        "Ce graphique permet de visualiser l’évolution du nombre de participants en fonction des années. Il "
+        "suffit dans un premier temps de sélectionner l’intervalle d’année souhaité. Si l’on sélectionne toutes "
+        "les années, de 1996 à 2016, on peut voir qu’en 1996, il y avait 2985 participants. Par la suite le "
+        "nombre de participants se situait autour de 3400 et 3490. En 2012, ce nombre s’affaiblit à hauteur de "
+        "3277 pour enfin arriver à 3598 en 2016."
+    ),
+    
+    html.P(
+        "Ce graphique permet de visualiser le nombre de médailles par année. On peut voir une croissance "
+        "quasiment linéaire de ce nombre. Cela peut être dû notamment à l’expansion des Jeux Olympiques. "
+        "En effet avec l'ajout de nouveaux sports, disciplines et catégories. L'inclusion de ces nouvelles "
+        "épreuves a naturellement conduit à une augmentation du nombre total de médailles disponibles. "
+        "L'augmentation de la représentation des femmes dans les Jeux Olympiques, avec l'introduction de "
+        "nouvelles épreuves et disciplines féminines, a contribué à l'augmentation du nombre total de "
+        "médailles décernées."
+    ),
+
 ])
 
 ######### Créer les sections de la deuxième page ###########
 page2_layout = html.Div([  
     # Titre
-    html.H1("Partie 2 - XXXX"),
+    html.H1("Partie 2 - Analyse de l'âge et des mensurations"),
+    
     # Section Description
     html.H2("Description", id='accueil'),
-    html.P("Bienvenue dans le tableau de bord des Jeux olympiques. Explorez les données et découvrez des informations intéressantes."),
+    html.P("Bienvenue dans la deuxième partie du tableau de bord des Jeux olympiques. Cette section est dédiée à l'analyse des données relatives à l'âge et aux mensurations des athlètes participant aux Jeux olympiques."),
     
-        
+    html.P("L'objectif de cette analyse est de fournir des insights intéressants sur les tendances générales de l'âge, de la taille et du poids des athlètes, en se concentrant sur la distinction entre hommes et femmes."),
+    
+    html.P("Explorez ces graphiques interactifs pour découvrir des informations utiles sur les caractéristiques physiques des athlètes dans différentes disciplines."),
+    
     #### Moyennes taille, poids et age par genre
     html.H1("Analyse des caractéristiques moyennes par sport et par année"),
 
@@ -181,19 +314,33 @@ page2_layout = html.Div([
     # Graphiques générés par callback
     html.Div([
         # Graphiques pour les hommes
-        html.Div([dcc.Graph(id='average-height-chart-M', style={'width': '33%', 'display': 'inline-block'}),
-                  dcc.Graph(id='average-weight-chart-M', style={'width': '33%', 'display': 'inline-block'}),
-                  dcc.Graph(id='average-age-chart-M', style={'width': '33%', 'display': 'inline-block'}),
-        ], style={'display': 'flex'}),
+        html.Div([
+            html.H2("Concernant les hommes", style={'text-align': 'center'}),
+            
+            dcc.Graph(id='average-height-chart-M', style={'width': '33%', 'display': 'inline-block'}),
+            dcc.Graph(id='average-weight-chart-M', style={'width': '33%', 'display': 'inline-block'}),
+            dcc.Graph(id='average-age-chart-M', style={'width': '33%', 'display': 'inline-block'}),
+        ]),
+        
+        # Commentaires pour les graphiques des hommes
+        html.P("Les basketteurs ont la plus grande taille, suivis des volleyeurs et enfin des joueurs de water polo. Les athlètes les plus petits pratiquent le trampoline."),
+        html.P("En termes de poids, les basketteurs, les handballeurs et les joueurs de water-polo sont les plus lourds, tandis que les athlètes de trampoline sont les plus légers."),
+        html.P("En ce qui concerne l'âge, les cavaliers, les tireurs et les joueurs de beach-volley sont parmi les plus âgés, tandis que les joueurs de football sont parmi les plus jeunes."),
+    ]),
 
-        # Graphiques pour les femmes
-        html.Div([dcc.Graph(id='average-height-chart-F', style={'width': '33%', 'display': 'inline-block'}),
-                  dcc.Graph(id='average-weight-chart-F', style={'width': '33%', 'display': 'inline-block'}),
-                  dcc.Graph(id='average-age-chart-F', style={'width': '33%', 'display': 'inline-block'}),
-        ], style={'display': 'flex'}),
+    # Graphiques pour les femmes
+    html.Div([
+        html.H2("Concernant les femmes", style={'text-align': 'center'}),
+        
+        dcc.Graph(id='average-height-chart-F', style={'width': '33%', 'display': 'inline-block'}),
+        dcc.Graph(id='average-weight-chart-F', style={'width': '33%', 'display': 'inline-block'}),
+        dcc.Graph(id='average-age-chart-F', style={'width': '33%', 'display': 'inline-block'}),
     ]),
     
-    
+    # Commentaires pour les graphiques des femmes
+    html.P("Les basketteuses, les volleyeuses et les joueuses de beach-volley ont la plus grande taille, tandis que les gymnastes ont la plus petite."),
+    html.P("En termes de poids, les basketteuses, les handballeuses et les volleyeuses sont les plus lourdes, tandis que les gymnastes sont les plus légères."),
+    html.P("En ce qui concerne l'âge, les cavalières, les tireuses et les joueuses de beach-volley sont parmi les plus âgées, tandis que les gymnastes sont parmi les plus jeunes."),
 ])
 
 
@@ -202,10 +349,13 @@ page2_layout = html.Div([
 ######### Créer les sections de la troisième page ###########
 page3_layout = html.Div([
     # Titre
-    html.H1("Partie 3 - XXXX"),
+    html.H1("Partie 3 - Prédiction de médaille"),
     # Section Description
     html.H2("Description", id='accueil'),
-    html.P("Bienvenue dans le tableau de bord des Jeux olympiques. Explorez les données et découvrez des informations intéressantes."),
+    
+    html.P("Bienvenue dans la troisième partie du tableau de bord des Jeux olympiques. Dans cette section, vous avez la possibilité de faire des prédictions de médailles pour un athlète en particulier."),
+    
+    html.P("Utilisez les champs ci-dessous pour entrer l'âge, la taille et le poids de l'athlète. Ensuite, cliquez sur le bouton 'Prédire' pour obtenir les probabilités de remporter une médaille dans chaque catégorie (Bronze, Argent, Or) selon deux modèles de machine learning : Régression Logistique et Random Forest."),
     
     dcc.Input(id='input-age', type='number', placeholder='Age'),
     dcc.Input(id='input-height', type='number', placeholder='Height'),
@@ -217,10 +367,19 @@ page3_layout = html.Div([
     html.Div(id='prediction-output-lr'),
     dcc.Graph(id='probability-bar-chart-lr'),  # Graphique à barres pour la Régression Logistique
     
+html.Div("Le graphique ci-dessus illustre les chances de médailles suivant les mensurations et "
+         "l'âge de l'utilisateur à l'aide d'une Régression Logistique. On remarque que les probabilités ne fluctuent pas "
+         "beaucoup et semblent erronées, cela est dû à l'accuracy qui est de 0.3."),  # Commentaire sous le graphique
+
+    
     # Section pour Random Forest
     html.H2("Random Forest", id='random-forest'),
     html.Div(id='prediction-output-rf'),
     dcc.Graph(id='probability-bar-chart-rf'),  # Graphique à barres pour le Random Forest
+
+html.Div("Le graphique ci-dessus illustre les chances de médailles suivant les mensurations et "
+         "l'âge de l'utilisateur à l'aide d'un random forest. On remarque que les probabilités ici fluctuent bien au regard d'un changement dans les mensurations, "
+         "c'est notamment grâce à l'accuracy qui est de 0.9."),  # Commentaire sous le graphique
 
 ])
 
@@ -445,3 +604,5 @@ def predict_medal_with_probabilities_and_chart_rf(n_clicks, age, height, weight)
 ##########  Test  ##########
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
